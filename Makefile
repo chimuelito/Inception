92c790d781dd95fdfc86f1bf8f007d91fbe4871d
Nical <nicolas.catriens@gmail.com>
2022-02-13 17:32:00 +0100

    wordpress is working. This is a miracle.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 launch:
-	cd srcs && mkdir -p wp_volume && docker-compose up --build -d
+	cd srcs && mkdir -p wp_volume && mkdir -p mariadb_volume && docker-compose up --build -d
 
 clean:
 	cd srcs && docker-compose down -v
 
 fclean: clean
 	docker system prune
-	cd srcs && sudo rm -rf wp_volume
+	cd srcs && sudo rm -rf wp_volume mariadb_volume
--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -2,8 +2,10 @@
 
 # We need to start php once, then stop it to avoid error of "instance of php already running..."
 # when launching php for a not stopping command. 
-chmod -R 755 /shared_data
-chown www-data:www-data /shared_data
+mv wordpress/* /var/www/wordpress
+rm -rf wordpress
+chmod -R 755 /var/www/wordpress
+chown www-data:www-data /var/www/wordpress
 service php7.3-fpm start
 service php7.3-fpm stop
 php-fpm7.3 -F -R # -F: no daemonize; -R: run as root
