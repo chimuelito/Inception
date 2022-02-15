399087e20d59d0126f087319b7bb504a4839de04
Nical <nicolas.catriens@gmail.com>
2022-02-15 20:02:35 +0100

    in wordpress, wp-config.php is properly scripted

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
+CONF_FILE=/var/www/wordpress/wp-config.php
+
 # We need to start php once, then stop it to avoid error of "instance of php already running..."
 # when launching php for a not stopping command. 
-mv wordpress/* /var/www/wordpress
-rm -rf wordpress
-chmod -R 755 /var/www/wordpress
-chown www-data:www-data /var/www/wordpress
+if [[ -d wordpress ]]
+then
+	mv wordpress/* /var/www/wordpress
+	rm -rf wordpress
+	chmod -R 755 /var/www/wordpress
+	chown www-data:www-data /var/www/wordpress
+
+	sed -i "s/DB_NAME/$WP_DB_NAME/2" $CONF_FILE 
+	sed -i "s/DB_USER/$WP_DB_USER/2" $CONF_FILE 
+	sed -i "s/DB_PASSWORD/$MYSQL_PASSWORD/2" $CONF_FILE 
+fi
+
 service php7.3-fpm start
 service php7.3-fpm stop
 php-fpm7.3 -F -R # -F: no daemonize; -R: run as root
--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -22,13 +22,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'DB_NAME' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'wordpress_user' );
+define( 'DB_USER', 'DB_USER' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_PASSWORD', 'DB_PASSWORD' );
 
 /** Adresse de l’hébergement MySQL. */
 define( 'DB_HOST', 'mariadb:3306' );
