76df679cef6c7de404051dc666a4efa27bdff49f
Nical <nicolas.catriens@gmail.com>
2022-02-19 13:52:42 +0100

    OK now. Reorganised wp image, simplified db setup.

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -3,19 +3,17 @@
 CONF_FILE=/var/www/html/wordpress/wp-config.php
 
 service php7.3-fpm start
+cd /var/www/html/wordpress/
 # We need to start php once, then stop it to avoid error of "instance of php already running..."
 # when launching php for a not stopping command. 
 #chmod -R 755 /var/www/html/wordpress
+rm wp-config-sample.php
 
 sed -i "s/DB_NAME/$WP_DB_NAME/2" $CONF_FILE 
 sed -i "s/DB_USER/$WP_DB_USER/2" $CONF_FILE 
 sed -i "s/DB_PASSWORD/$MYSQL_PASSWORD/2" $CONF_FILE 
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-cd /var/www/html/wordpress/
-wp core install --url=localhost.com --title=SuperWebsite --admin_user=superman --admin_password=superman --admin_email=super@man.com --skip-email --allow-root
+wp core install --url=localhost --title=SuperWebsite --admin_user=superman --admin_password=superman --admin_email=super@man.com --skip-email --allow-root
 wp user create bob bob@mail.com --role=author --user_pass=bob --allow-root 
 
 #service php7.3-fpm restart
